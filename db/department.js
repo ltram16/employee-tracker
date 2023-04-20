132989a391da0b2a87bba92ee8e55d2884863511
ltram16 <ltram16@gmail.com>
2023-04-19 20:59:36 -0700

    edited department.js file

--- db/department.js
+++ db/department.js
@@ -1,5 +1,57 @@
 const db = require("./db/connection");
+const inquirer  = require("inquirer");
 
 async function viewAllDepartments() {
-    db.query('SELECT * FROM department').promise()
-}
+    try {
+        const departments =
+            await db.promise().query('SELECT * FROM department')
+        return departments[0];
+    } catch (err) {
+        console.log(err)
+    }
+};
+
+async function addDepartment() {
+    try {
+        const departments = await viewAllDepartments();
+        const { name } =
+        await inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "What is the name of the department you would like to add?"
+            }
+        ])
+        await db.query(`INSERT INTO department (name) VALUES ("${name})`)
+        const addDepartment = await viewAllDepartments();
+        return addDepartment;
+    } catch (err) {
+        console.log(err);
+    };
+};
+
+async function delDepartment() {
+    try {
+        const departments = await viewAllDepartments();
+        const { id } =
+        await inquirer.prompt([
+            {
+                type: "list",
+                name: "id",
+                message: "What is the name of the department you would like to delete?",
+                choices: departments.map((department) => {
+                    return {
+                        name: department.name,
+                        value: department.id
+                    }
+                })
+            }
+        ])
+        await db.query(`DELETE FROM department WHERE id = ${id}`);
+        return await viewAllDepartments();
+    } catch (err) {
+        console.log(err);
+    };
+};
+       
+module.exports = {viewAllDepartments, addDepartment, delDepartment};
